898d4796fe70a29b3507b2f4732e0671087aa7be
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-06-28 02:24:10 +0900

    popup/indexにhello worldを実装

---
+++ src/popup/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Page: React.FC = () => {
+  return (
+    <>
+      <h1>Hello World!</h1>
+    </>
+  )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
